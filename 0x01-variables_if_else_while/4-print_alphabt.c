99593a316233a1b4186a811312c9a67f6e8c769f
mostafataha12 <mostafataha5454@gmail.com>
2023-08-11 08:45:00 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putcher(c);
+			c++;
+		}
+	putcher('\n');
+	return (0);
+}
